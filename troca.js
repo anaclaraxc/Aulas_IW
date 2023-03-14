9bc6bb36d3709682716d4a8b7c0bc3f270ba6a7e
Ana Clara Cordeiro <100931939+anaclaraxc@users.noreply.github.com>
2023-03-14 12:46:34 -0300

    14-03

---
+++ troca.js
@@ -0,0 +1,14 @@
+function pegavalor(botoes){
+
+    let val = botoes.value
+
+    alert(val)
+     case 1: console.log("Página 1")
+     break
+     case 2: console.log("Página 2")
+     break
+     case 3: console.log("Página 3")
+     break
+     default: console.log("Nnenhuma das opções")
+     break
+}
